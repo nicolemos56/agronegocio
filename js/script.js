4edb9b5665cf446ed95bbea9d86855526d12fe39
Manuel <nicocohen56@gmail.com>
2024-06-10 16:31:41 +0100

    tornei a pagina responsiva usando o media queries e flexbox

--- js/script.js
+++ js/script.js
@@ -3,10 +3,19 @@ const form = document.getElementById('contact-form');
 form.addEventListener('submit', (event) => {
     event.preventDefault(); // Impede o envio padrão do formulário
 
-    /
     
     alert('Formulário enviado com sucesso!');
 
     // Limpa os campos do formulário
     form.reset();
 });
+
+const toggleButton = document.querySelector('.navbar-toggle');
+const navbarMenu = document.querySelector('.navbar ul');
+
+toggleButton.addEventListener('click', () => {
+    toggleButton.classList.toggle('active');
+    navbarMenu.classList.toggle('mostrar');
+});
+
+
